a361bfc6c3cdc25fef1b6914634e9b4bde27f284
Yumeng Zhang <zhangym9717@hotmail.com>
2020-12-25 17:14:48 +0800

    timer functionality completed

--- src/components/timer_box/index.js
+++ src/components/timer_box/index.js
@@ -1,28 +1,91 @@
 import React, {useState} from "react";
-import {TimerContainer, TimerCircle, TimerButtons} from "./timer_elements";
+import {TimerContainer, TimerInitialButton, TimerRunningButtons, TimerPausedButtons, TimerCompletedButton} from "./timer_elements";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 function TimerBox (props){
+    // **===== Timer state management ===**
+    let timerInitialState = 0;
+    let timerRunningState = 1;
+    let timerPausedState = 2;
+    let timerCompletedState = 3;
+    // === End of timer state management ==
+
+    // **===== React State Management ===**
     let totalTime = props.time;
     const [timeRemaining, setTimeRemaining] = useState(parseInt(totalTime));
+    const [currentState, setTimerState] = useState(timerInitialState);
+    const [timer, setTimer] = useState(0);
+    // == End of React State Management ===
+
 
     let timeRun = 0;
     const startTimer = () => {
-        console.log(timeRemaining);
-        let timer = window.setInterval(() => {
+        // update timer state
+        setTimerState(timerRunningState);
+        // update timeRemaining
+        timeRun = totalTime - timeRemaining;
+        setTimer(window.setInterval(() => {
             setTimeRemaining(prevTime => prevTime - 1);
             timeRun += 1;
-            console.log('hh' + timeRun);
             if (timeRun == totalTime) {
-                console.log('hhh');
-                clearInterval(timer);
+                console.log('time is up');
+                setTimer(prevState => clearInterval(prevState));
+                setTimerState(timerCompletedState);
             }
-        }, 1000);
+        }, 1000));
     }
+
+    const pauseTimer = () => {
+        // cancel timer
+        setTimer(prev => clearInterval(prev));
+        // update timer state
+        setTimerState(timerPausedState);
+    }
+
+    const restartTimer = () => {
+        // update timer state
+        setTimerState(timerInitialState);
+        // set timer
+        setTimer(prev => clearInterval(prev));
+        // set timeRemaining
+        setTimeRemaining(totalTime);
+    }
+
+    const getTime = () => {
+        let hour = Math.floor(timeRemaining / 3600);
+        let hourStr = hour.toString().length === 1 ? '0' + hour.toString() : hour.toString();
+        let min = Math.floor(timeRemaining % 3600 / 60);
+        let minStr = min.toString().length === 1 ? '0' + min.toString() : min.toString();
+        let sec = timeRemaining % 3600 % 60;
+        let secStr = sec.toString().length === 1 ? '0' + sec.toString() : sec.toString();
+        return hourStr + ":" + minStr + ":" + secStr;
+    }
+
+    
 
     return (
         <TimerContainer>
-            <TimerCircle time={totalTime} timeRemaining={timeRemaining}/>
-            <TimerButtons onClick={startTimer}></TimerButtons>
+            <div style={{width: "200px", height: "200px"}}>
+                <CircularProgressbar 
+                    value={parseInt(timeRemaining)/parseInt(totalTime) * 100} 
+                    text={getTime()}
+                    strokeWidth="10"
+                    styles={buildStyles({
+                        strokeLinecap: 'butt',
+                        textSize: '15px',
+                        pathTransitionDuration: 0.5,
+                        pathColor: "rgba(233, 222, 244, 0.8)",
+                        textColor: 'rgb(112,112,112)',
+                        trailColor: 'transparent',
+                        backgroundColor: 'red',
+                    })}
+                />
+            </div>
+            {currentState === timerInitialState && <TimerInitialButton onClick={startTimer} />}
+            {currentState === timerRunningState && <TimerRunningButtons pauseEvent={pauseTimer} restartEvent={restartTimer} />}
+            {currentState === timerPausedState && <TimerPausedButtons startEvent={startTimer} restartEvent={restartTimer} />}
+            {currentState === timerCompletedState && <TimerCompletedButton restartEvent={restartTimer} />}
         </TimerContainer>
     );
 }
--- src/components/timer_box/timer_elements.js
+++ src/components/timer_box/timer_elements.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import React from "react";
-import {AiFillCaretRight} from "react-icons/ai"
+import {AiFillCaretRight, AiOutlinePause, AiOutlineReload} from "react-icons/ai"
 
 export const TimerContainer = styled.div`
     width: 250px;
@@ -14,63 +14,45 @@ export const TimerContainer = styled.div`
     justify-content: center;
 `;
 
-export const TimerCircle = (props) => {
-    let hour = Math.floor(props.timeRemaining / 3600);
-    let hourStr = hour.toString().length === 1 ? '0' + hour.toString() : hour.toString();
-    let min = Math.floor(props.timeRemaining % 3600 / 60);
-    let minStr = min.toString().length === 1 ? '0' + min.toString() : min.toString();
-    let sec = props.timeRemaining % 3600 % 60;
-    let secStr = sec.toString().length === 1 ? '0' + sec.toString() : sec.toString();
-    let d1 = 126;
-    let d2 = 90;
-    return (
-        <BottomCircle>
-            <MiddleCircle d1={d1} d2={d2}>
-                <TopCircle>{hourStr + ":" + minStr + ":" + secStr}</TopCircle>
-            </MiddleCircle>
-        </BottomCircle>
-    );
-}
-
-const BottomCircle = styled.div`
-    background-color: rgb(233, 222, 244);
-    border-radius: 50%;
-    width: 180px;
-    height: 180px;
-    box-shadow: 0px 3px 6px rgba(0,0,0,0.2);
-`;
-
-const MiddleCircle = styled.div`
-    background: linear-gradient(${props => props.d1 + "deg"}, transparent 50%, white 50%),
-    linear-gradient(${props => props.d2 + "deg"}, white 50%, transparent 50%);
-    border-radius: 50%;
-    width: 180px;
-    height: 180px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`;
-
-const TopCircle = styled.div`
-    width: 130px;
-    height: 130px;
-    border-radius: 50%;
-    background-color: white;
-    box-shadow: 0px 3px 6px rgba(0,0,0,0.2);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    font-size: 20px;
-`;
-
-export const TimerButtons = (props) => {
+export const TimerInitialButton = (props) => {
     return (
         <TimerButtonContainer onClick={props.onClick}>
             <AiFillCaretRight size="20px" />
         </TimerButtonContainer>
     );
+}
+
+export const TimerRunningButtons = (props) => {
+    return (
+        // <div style="display: flex, flex-direction: row, ">
+        <ButtonRowContainer>
+            <TimerButtonContainer onClick={props.pauseEvent}>
+                <AiOutlinePause size="20px" />
+            </TimerButtonContainer>
+            <div style={{width: "20px",}}></div>
+            <TimerButtonContainer onClick={props.restartEvent}>
+                <AiOutlineReload size="20px" />
+            </TimerButtonContainer>
+        </ButtonRowContainer>
+    );
+}
+
+export const TimerPausedButtons = (props) => {
+    return <ButtonRowContainer>
+        <TimerButtonContainer onClick={props.startEvent}>
+                <AiFillCaretRight size="20px" />
+            </TimerButtonContainer>
+            <div style={{width: "20px",}}></div>
+            <TimerButtonContainer onClick={props.restartEvent}>
+                <AiOutlineReload size="20px" />
+            </TimerButtonContainer>
+    </ButtonRowContainer>
+}
+
+export const TimerCompletedButton = (props) => {
+    return <TimerButtonContainer onClick={props.restartEvent}>
+        <AiOutlineReload size="20px" />
+    </TimerButtonContainer>
 }
 
 const TimerButtonContainer = styled.button`
@@ -87,4 +69,11 @@ const TimerButtonContainer = styled.button`
     align-items: center;
     outline: none;
     border: none;
+`;
+
+const ButtonRowContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
 `;
